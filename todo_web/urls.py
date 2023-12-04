c59435154666abe48e08f9c90615d438d31cd890
MalkovGN <malkovgn@gmail.com>
2023-12-04 13:31:22 +0300

    Added redirecting to login page if user is not authorized

--- todo_web/urls.py
+++ todo_web/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from todo_web.views import ItemsListView, ItemUpdateView, ItemCreateView, ItemDeleteView
 
 app_name = 'items'
 
 urlpatterns = [
-    path('all/', ItemsListView.as_view(), name='all_items'),
-    path('item/<int:pk>/', ItemUpdateView.as_view(), name='item_detail'),
-    path('create/', ItemCreateView.as_view(), name='item_create'),
-    path('delete/<int:pk>/', ItemDeleteView.as_view(), name='item_delete'),
+    path('all/', login_required(ItemsListView.as_view()), name='all_items'),
+    path('item/<int:pk>/', login_required(ItemUpdateView.as_view()), name='item_detail'),
+    path('create/', login_required(ItemCreateView.as_view()), name='item_create'),
+    path('delete/<int:pk>/', login_required(ItemDeleteView.as_view()), name='item_delete'),
 ]
